326bfa3e6875b276b23122a13a7f56819a602468
hong <hong@telcoin_1>
2014-10-30 17:41:15 +0900

    t

--- test4/src/test/test.java
+++ test4/src/test/test.java
@@ -5,6 +5,8 @@ public abstract class test {
 	public static void main(String[] args) {
 		System.out.println(" hmm...");
 		System.out.println(" hmm...2");
+		System.out.println(" hmm...2");
+		System.out.println(" hmm...2");
 
 	}
 
