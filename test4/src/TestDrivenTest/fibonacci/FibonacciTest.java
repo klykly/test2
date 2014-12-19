092a74809d21042e20bbcb702bc42bd2639c4aec
hong <hong@telcoin_1>
2014-12-19 17:41:01 +0900

    9

--- test4/src/TestDrivenTest/fibonacci/FibonacciTest.java
+++ test4/src/TestDrivenTest/fibonacci/FibonacciTest.java
@@ -2,7 +2,7 @@ package TestDrivenTest.fibonacci;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class FibonacciTest {
 	
@@ -11,6 +11,23 @@ public class FibonacciTest {
 		else if ( n <= 2 ) return 1;
 		return fib(n-1) + fib(n-2);
 	}
+	
+	@BeforeClass
+	public static void start(){
+		System.out.println("START----------------------------------------------");
+	}
+	@AfterClass
+	public static void end(){
+		System.out.println("END------------------------------------------------");
+	}
+	@Before
+	public void setUp() {
+		System.out.println("BEFORE!!");
+	}
+	@After
+	public void after() {
+		System.out.println("AFTER!!");
+	}
 	@Test
 	public void testFibonacci(){
 		int cases[][] = { {0,0}, {1,1}, {2,1}, {3,2}, {20,6765}	};
@@ -18,5 +35,11 @@ public class FibonacciTest {
 			assertEquals(cases[i][1], fib(cases[i][0]));
 		}
 	}
-
+	@Test
+	public void testFibonacci2(){
+		int cases[][] = { {0,0}, {1,1}, {2,1}, {3,2}, {20,6765}	};
+		for( int i = 0; i < cases.length; i++){
+			assertEquals(cases[i][1], fib(cases[i][0]));
+		}
+	}
 }
